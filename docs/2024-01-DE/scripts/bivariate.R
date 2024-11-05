96c51e6dddb9df9165531624489c7ebd771240df
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2024-11-05 20:09:59 +0530

    update - linear regression

--- docs/2024-01-DE/scripts/bivariate.R
+++ docs/2024-01-DE/scripts/bivariate.R
@@ -120,6 +120,123 @@ xyplot(BPSysAve ~ Age | Gender,
        subset = Race1 == "White" & Age >= 20)
 
 
+## Regression examples (Nov 5, 2024)
+
+library(carData)
+
+xyplot(weight ~ height, data = Davis, grid = TRUE, 
+       groups = sex, auto.key = TRUE)
+
+xyplot(pctUrban ~ ppgdp, data = UN, grid = TRUE)
+xyplot(pctUrban ~ log(ppgdp), data = UN, grid = TRUE)
+
+
+## function to calculate a-hat and b-hat
+
+s2 <- function(x) {
+  m <- mean(x)
+  mean((x - m)^2)
+}
+
+cxy <- function(x, y) {
+  mx <- mean(x)
+  my <- mean(y)
+  mean((x - mx) * (y - my))
+}
+
+lsfit <- function(x, y) {
+  if (length(x) != length(y)) stop("Lengths and x and y do not match")
+  skip <- is.na(x) | is.na(y)
+  x <- x[!skip]
+  y <- y[!skip]
+  mx <- mean(x)
+  my <- mean(y)
+  s2x <- s2(x)
+  covxy <- cxy(x, y)
+  b_hat <- covxy / s2x
+  a_hat <- my - b_hat * mx
+  c(a = a_hat, b = b_hat)
+}
+
+
+
+abDavis <- with(Davis, lsfit(x = height, y = weight))
+
+abDavis
+
+## Model weight = a * 1 + b * height + error
+coef(lm(weight ~ 1 + height, data = Davis))
+
+xyplot(weight ~ height, data = Davis, grid = TRUE, 
+       groups = sex, auto.key = TRUE,
+       abline = abDavis)
+
+xyplot(weight ~ height, data = Davis, grid = TRUE, 
+       groups = sex, auto.key = TRUE, 
+       type = c("p", "r"),
+       abline = abDavis)
+
+
+
+abUN <- with(UN, lsfit(x = log(ppgdp), y = pctUrban))
+
+xyplot(pctUrban ~ log(ppgdp), data = UN, grid = TRUE,
+       abline = abUN)
+
+
+nhSubM <- subset(NHANES, Race1 == "White" & Age >= 20 & Gender == "male")
+nhSubF <- subset(NHANES, Race1 == "White" & Age >= 20 & Gender == "female")
+
+abNH.M <- with(nhSubM, lsfit(x = Age, y = BPSysAve))
+xyplot(BPSysAve ~ Age, data = nhSubM, grid = TRUE, abline = abNH.M)
+
+abNH.F <- with(nhSubF, lsfit(x = Age, y = BPSysAve))
+xyplot(BPSysAve ~ Age, data = nhSubF, grid = TRUE, abline = abNH.F)
+
+
+## How can we do Least Absolute Error
+
+ladfit <- function(x, y) {
+  if (length(x) != length(y)) stop("Lengths and x and y do not match")
+  skip <- is.na(x) | is.na(y)
+  x <- x[!skip]
+  y <- y[!skip]
+  lambda <- function(par) {
+    a <- par[1]
+    b <- par[2]
+    sum(abs(y - a - b * x))
+  }
+  opt <- optim(par = c(0, 0), fn = lambda)
+  opt$par
+}
+
+abDavisLAD <- with(Davis, ladfit(x = height, y = weight))
+
+xyplot(weight ~ height, data = Davis, grid = TRUE, 
+       groups = sex, auto.key = TRUE, 
+       type = c("p", "r"),
+       abline = abDavisLAD)
+
+
+abUNLAD <- with(UN, ladfit(x = log(ppgdp), y = pctUrban))
+
+xyplot(pctUrban ~ log(ppgdp), data = UN, grid = TRUE,
+       abline = abUNLAD, type = c("p", "r"))
+
+xyplot(pctUrban ~ log(ppgdp), data = UN, grid = TRUE,
+       abline = abUNLAD, type = c("p", "smooth"), col.line = "red")
+
+
+abNH.MLAD <- with(nhSubM, ladfit(x = Age, y = BPSysAve))
+xyplot(BPSysAve ~ Age, data = nhSubM, grid = TRUE, 
+       type = c("p", "r"), abline = abNH.MLAD)
+
+abNH.FLAD <- with(nhSubF, ladfit(x = Age, y = BPSysAve))
+xyplot(BPSysAve ~ Age, data = nhSubF, grid = TRUE, 
+       type = c("p", "r"), abline = abNH.FLAD)
+
+xyplot(BPSysAve ~ Age, data = nhSubF, grid = TRUE, 
+       type = c("p", "smooth"), abline = abNH.FLAD, col.line = "red")
 
 
 
